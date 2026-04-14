ff2d0a39e44748d4e48ea41eaba3defbc02d34b4
Mateusz Padlo <matiprokoxuxd5@gmail.com>
2026-04-14 08:48:04 +0200

    feat: dodano oznaczanie zada jako ukonczonych

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ let lista = document.getElementById("task-list");
 
 przycisk.onclick = function() {
     let li = document.createElement("li");
-    li.textContent = pole.value;
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    let tekst = document.createTextNode(pole.value);
+
+    checkbox.onchange = function() {
+        if (checkbox.checked) {
+            li.style.textDecoration = "line-through";
+        } else {
+            li.style.textDecoration = "none";
+        }
+    };
+
+    li.appendChild(checkbox);
+    li.appendChild(tekst);
+
     lista.appendChild(li);
 };
